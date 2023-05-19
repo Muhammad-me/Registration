967f8fa3efaab14ff277f2fb6f17b0f62295e0a3
Oybek Xaitboyev <oybek2000ox@gmail.com>
2023-05-19 21:56:08 +0500

    created

---
+++ src/ArrowDown.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function ArrowDown() {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="white"
+        strokeWidth={1.5}
+        stroke="#fff"
+        className="w-8 h-8 mx-auto animate-bounce mt-10"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3"
+        />
+      </svg>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,126 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import ArrowDown from "@/ArrowDown";
+import { Button, Group, Input, Modal, Table, TextInput } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import axios from "axios";
+import { Inter } from "next/font/google";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [opened, { open, close }] = useDisclosure(false);
+  const [full, setFull] = useState("");
+  const [date, setDate] = useState("");
+  const [reas, setReas] = useState("");
+  const [addres, setAddres] = useState("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const data = {
+      IsmFamiliya: full,
+      Sana: date,
+      Address: addres,
+      KasallikSababi: reas,
+    };
+    axios
+      .post(
+        "https://sheet.best/api/sheets/01ff3c0c-fc53-4627-a820-4352213546e9",
+        data
+      )
+      .then((res) => {
+        setFull(""), setDate(""), setAddres(""), setReas("");
+      });
+    console.log(data);
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="bg-gradient-to-r from-sky-500 to-indigo-500 w-full h-screen flex items-center justify-center">
+      <div>
+        <Modal
+          opened={opened}
+          onClose={close}
+          title="Royxatga qo'shish"
+          onSubmit={(e) => handleSubmit(e)}
+        >
+          <TextInput
+            placeholder="Sizning ismi familiyasi"
+            label="Ismi Familiyasi"
+            variant="filled"
+            mt={10}
+            onChange={(e) => setFull(e.target.value)}
+            value={full}
+            required
+          />
+          <Input.Wrapper label="Tug'ilgan sanasi" mt={20}>
+            <Input
+              placeholder="Sizning tug'ilgan sanangiz"
+              type="date"
+              variant="filled"
+              onChange={(e) => setDate(e.target.value)}
+              value={date}
+              required
             />
-          </a>
+          </Input.Wrapper>
+          <TextInput
+            placeholder="Joylashuvnu kiriting"
+            label="Manzil"
+            variant="filled"
+            mt={10}
+            onChange={(e) => setAddres(e.target.value)}
+            value={addres}
+            required
+          />
+          <TextInput
+            placeholder="Kasallik haqida"
+            label="Kasallik turi"
+            variant="filled"
+            mt={10}
+            onChange={(e) => setReas(e.target.value)}
+            value={reas}
+            required
+          />
+          <Group>
+            <Button
+              className="font-medium px-3 rounded-xl bg-blue-400 text-white ml-80 my-5"
+              onClick={(e) => handleSubmit(e)}
+            >
+              Jonatish
+            </Button>
+          </Group>
+        </Modal>
+        <div className="container mx-auto px-10 lg:w-[900px]">
+          <div className="flex items-center justify-between">
+            <Link href={"/"} className="font-bold text-3xl">
+              Reg
+            </Link>
+            <Group>
+              <Button
+                className="font-medium text-xl px-4 rounded-xl bg-blue-400 text-white"
+                onClick={open}
+              >
+                + Ro&apos;yxatga qo&apos;shish
+              </Button>
+            </Group>
+          </div>
+        </div>
+        <div className="container mx-auto px-16 text-center">
+          <h1 className="font-semibold text-2xl text-center mt-10 lg:mt-16 text-white">
+            Malumotlarni ko&apos;rish uchun bosing
+          </h1>
+          <div>
+            <ArrowDown />
+          </div>
+          <Link
+            href={
+              "https://docs.google.com/spreadsheets/d/15q4JwVOj1ouMgkZMKbhllVRez69eCTjIDy0w6e8cI2U/edit?usp=sharing"
+            }
+            className="text-2xl font-bold w-full"
+          >
+            <div className="cursor-pointer">Ko&apos;rish</div>
+          </Link>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
